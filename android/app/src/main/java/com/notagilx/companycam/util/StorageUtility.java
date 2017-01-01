b828cf8e9bf7b211b630a819deb4baaf61f493aa
TDM <tdm1184@gmail.com>
2017-01-01 16:12:49 -0600

    Convert to library; fix StorageUtility getNewFileName; bump version to 8

--- android/app/src/main/java/com/notagilx/companycam/util/StorageUtility.java
+++ android/app/src/main/java/com/notagilx/companycam/util/StorageUtility.java
@@ -1,14 +1,10 @@
 package com.notagilx.companycam.util;
 
-import android.content.Context;
-import android.net.Uri;
-import android.os.Environment;
-
-import java.io.File;
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+
+import org.parceler.apache.commons.lang.RandomStringUtils;
 
 /**
  * Created by keaton on 4/18/15.
@@ -17,8 +13,7 @@ public class StorageUtility {
 
     public static String getNewFileName() {
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date());
-        //return String.format("%s-%s.jpg", timeStamp, RandomStringUtils.randomAlphabetic(16));
-        return String.format("%s-%s.jpg", timeStamp, "RUEIWORUERUOW"); //TODO
+        return String.format("%s-%s.jpg", timeStamp, RandomStringUtils.randomAlphabetic(16));
     }
 
 }
