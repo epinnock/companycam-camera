845571ca2eed4c5bf56a2079f55530e599e3d28a
tdmaienschein <thomas@companycam.com>
2017-05-03 16:55:11 -0500

    Show previews of the Canny outlines; add more timing info

--- android/app/src/main/java/com/newcam/imageprocessing/DocumentScanOverlay.java
+++ android/app/src/main/java/com/newcam/imageprocessing/DocumentScanOverlay.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.Rect;
 import android.os.Build;
 import android.renderscript.Allocation;
@@ -14,11 +16,17 @@ import android.renderscript.ScriptIntrinsicYuvToRGB;
 import android.renderscript.Type;
 import android.view.View;
 
-import java.nio.ByteBuffer;
+import java.util.List;
 
-import boofcv.alg.filter.blur.BlurImageOps;
+import boofcv.alg.feature.detect.edge.CannyEdge;
+import boofcv.alg.filter.binary.BinaryImageOps;
+import boofcv.alg.filter.binary.Contour;
 import boofcv.android.ConvertBitmap;
+import boofcv.factory.feature.detect.edge.FactoryEdgeDetectors;
+import boofcv.struct.ConnectRule;
+import boofcv.struct.image.GrayS16;
 import boofcv.struct.image.GrayU8;
+import georegression.struct.point.Point2D_I32;
 
 /**
  * Created by dan on 5/1/17.
@@ -28,20 +36,28 @@ public class DocumentScanOverlay extends View implements CCCameraImageProcessor 
 
     protected Context context;
 
+    protected Bitmap bitmapOriginal;
+    protected Bitmap bitmapTransform;
+    protected Canvas canvasTransform;
+
     protected boolean didReceiveImageParams = false;
     protected int width;
     protected int height;
     protected int previewFormat;
 
-    protected Bitmap bitmapOriginal;
-    protected Bitmap bitmapTransform;
-    protected Canvas canvasTransform;
-    protected ByteBuffer bitmapTransformBytes;
+    protected boolean didPrepareBoof = false;
+    protected CannyEdge<GrayU8,GrayS16> canny;
+    protected GrayU8 boofOrigImg;
+    protected GrayU8 boofProcImg;
+    byte[] workBuffer;
 
-    protected boolean allocationOkay = false;
+    protected boolean didPrepareRenderScript = false;
     protected Allocation allocIn;
     protected Allocation allocOut;
     protected ScriptIntrinsicYuvToRGB yuvToRgbIntrinsic;
+
+    protected boolean didPrepareOverlay = false;
+
 
     public DocumentScanOverlay(Context context) {
         super(context);
@@ -66,12 +82,12 @@ public class DocumentScanOverlay extends View implements CCCameraImageProcessor 
         allocOut = Allocation.createTyped(rs, rgbaType.create(), Allocation.USAGE_SCRIPT);
 
         yuvToRgbIntrinsic.setInput(allocIn);
-        allocationOkay = true;
+        didPrepareRenderScript = true;
     }
 
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
     public void convertYUVToRGB(Bitmap target, byte[] data){
-        if(!allocationOkay){ return; }
+        if(!didPrepareRenderScript){ return; }
 
         allocIn.copyFrom(data);
         yuvToRgbIntrinsic.forEach(allocOut);
@@ -81,8 +97,7 @@ public class DocumentScanOverlay extends View implements CCCameraImageProcessor 
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        if(!didReceiveImageParams){ return; }
-        if(!allocationOkay){ return; }
+        if(!didPrepareOverlay){ return; }
 
         Rect rSrc = new Rect(0, 0, bitmapTransform.getWidth(), bitmapTransform.getHeight());
         Rect rDst = new Rect(0, 0, canvas.getWidth(), canvas.getHeight());
@@ -94,10 +109,12 @@ public class DocumentScanOverlay extends View implements CCCameraImageProcessor 
     @Override
     public void setBytes(byte[] dataOriginal) {
         if(!didReceiveImageParams){ return; }
+        if(!didPrepareBoof){ return; }
+        if(!didPrepareRenderScript){ return; }
 
         DEBUG_OUTPUT("Received bytes!");
 
-        long startMS = System.currentTimeMillis();
+        long step1MS = System.currentTimeMillis();
 
         convertYUVToRGB(bitmapOriginal, dataOriginal);
 
@@ -106,54 +123,51 @@ public class DocumentScanOverlay extends View implements CCCameraImageProcessor 
         canvasTransform.drawBitmap(bitmapOriginal, rSrc, rDst, null);
 
         //convert bitmap to boof U8; prepare boof U8 image for edges
-        GrayU8 boofOrigImg = ConvertBitmap.bitmapToGray(bitmapTransform, (GrayU8)null, null);
-        GrayU8 boofProcImg = boofOrigImg.createSameShape();
+        ConvertBitmap.bitmapToGray(bitmapTransform, boofOrigImg, workBuffer);
 
-        //CannyEdge<GrayU8,GrayS16> canny = FactoryEdgeDetectors.canny(2,true, true, GrayU8.class, GrayS16.class);
-        //canny.process(boofOrigImg,0.1f,0.3f,boofEdgeImg);
-        //List<EdgeContour> edgeContours = canny.getContours();
-        //List<Contour> contours = BinaryImageOps.contour(boofEdgeImg, ConnectRule.EIGHT, null);
+        long step2MS = System.currentTimeMillis();
 
-        BlurImageOps.median(boofOrigImg, boofProcImg, 8);
+        canny.process(boofOrigImg, 0.1f, 0.3f, boofProcImg);
+
+        long step3MS = System.currentTimeMillis();
+
+        List<Contour> contours = BinaryImageOps.contour(boofProcImg, ConnectRule.EIGHT, null);
+
+        long step4MS = System.currentTimeMillis();
 
         //convert edges image back to bitmap
-        byte[] workBuffer = ConvertBitmap.declareStorage(bitmapTransform, null);
-        ConvertBitmap.grayToBitmap(boofProcImg, bitmapTransform, workBuffer);
-
-        /*bitmapTransformBytes.rewind();
-        bitmapTransform.copyPixelsToBuffer(bitmapTransformBytes);
-        byte[] data = bitmapTransformBytes.array();
-
-        int maxVal = 0;
-        int maxX = 0;
-        int maxY = 0;
-
-        int index = 0;
-        for(int y = 0; y < bitmapTransform.getHeight(); y++){
-        for(int x = 0; x < bitmapTransform.getWidth(); x++){
-            int a = data[index*4+0] & 0xFF;
-            int r = data[index*4+1] & 0xFF;
-            int g = data[index*4+2] & 0xFF;
-            int b = data[index*4+3] & 0xFF;
-            int val = (int)(0.299f * (float)r + 0.587f * (float)g + 0.114f * (float)b);
-
-            if(val > maxVal){
-                maxVal = val;
-                maxX = x;
-                maxY = y;
-            }
-
-            index++;
-        }}
+        //byte[] workBuffer = ConvertBitmap.declareStorage(bitmapTransform, null);
+        //ConvertBitmap.grayToBitmap(boofOrigImg, bitmapTransform, workBuffer);
 
         Paint paint = new Paint();
-        paint.setStyle(Paint.Style.FILL);
-        paint.setColor(Color.argb(128, 0,255,0));
-        canvasTransform.drawRect(maxX - 5, maxY - 5, maxX + 5, maxY + 5, paint);*/
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(Color.argb(255, 0,255,0));
 
-        long endMS = System.currentTimeMillis();
-        DEBUG_OUTPUT("Finished processing: " + (endMS - startMS) + " ms");
+        for(Contour c : contours){
+            Path cpath = new Path();
 
+            int i = 0;
+            for(Point2D_I32 p : c.external) {
+                if(i == 0) {
+                    cpath.moveTo(p.getX(), p.getY());
+                }else {
+                    cpath.lineTo(p.getX(), p.getY());
+                }
+                i++;
+            }
+
+            canvasTransform.drawPath(cpath, paint);
+        }
+
+        long step5MS = System.currentTimeMillis();
+
+        DEBUG_OUTPUT("Finished processing: ");
+        DEBUG_OUTPUT(" - Image prep: " + (step2MS - step1MS) + " ms");
+        DEBUG_OUTPUT(" - Canny:      " + (step3MS - step2MS) + " ms");
+        DEBUG_OUTPUT(" - Contours:   " + (step4MS - step3MS) + " ms");
+        DEBUG_OUTPUT(" - Draw lines: " + (step5MS - step4MS) + " ms");
+
+        didPrepareOverlay = true;
         this.invalidate();
     }
 
@@ -164,17 +178,21 @@ public class DocumentScanOverlay extends View implements CCCameraImageProcessor 
         this.width = width;
         this.height = height;
         this.previewFormat = previewFormat;
+        didReceiveImageParams = true;
 
         bitmapOriginal = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-        bitmapTransform = Bitmap.createBitmap(512, 512, Bitmap.Config.ARGB_8888);
+        bitmapTransform = Bitmap.createBitmap(256, 256, Bitmap.Config.ARGB_8888);
         canvasTransform = new Canvas(bitmapTransform);
 
-        int bytes = bitmapTransform.getByteCount();
-        bitmapTransformBytes = ByteBuffer.allocate(bytes);
+        canny = FactoryEdgeDetectors.canny(2, true, true, GrayU8.class, GrayS16.class);
+        boofOrigImg = ConvertBitmap.bitmapToGray(bitmapTransform, (GrayU8)null, null);
+        boofProcImg = boofOrigImg.createSameShape();
+        workBuffer = ConvertBitmap.declareStorage(bitmapTransform, null);
+        didPrepareBoof = true;
 
         //NV21: YUV 12 bits per pixel
         prepareRenderScriptYUVToRGB(width*height*3/2);
 
-        didReceiveImageParams = true;
+
     }
 }
