8980c5a14d8d31d1b7658dd908070ef09363d24a
tdmaienschein <thomas@companycam.com>
2017-06-16 16:37:17 -0500

    Get rid of BoofCV and old scanner

--- android/app/src/main/java/com/newcam/imageprocessing/utils/CameraParams.java
+++
@@ -1,8 +0,0 @@
-package com.newcam.imageprocessing.utils;
-
-public class CameraParams {
-	public float fovy = 60.0f;
-	public float aspect = 1.0f;
-	public float imgw = 100.0f;
-	public float imgh = 100.0f;
-}
