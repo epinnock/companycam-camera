72f35a18db3f74b53fb2f5c7a63d041728c722e2
Jason Gaare <jason.d.gaare@gmail.com>
2017-10-17 15:47:48 -0500

    added prefix CCM to image helpers

--- ios/UIImage+CCMHelper.m
+++ ios/UIImage+CCMHelper.m
@@ -1,138 +1,138 @@
 //
-//  UIImage+CCHelper.m
+//  UIImage+CCMHelper.m
 //  newcam
 //
 //  Created by Matt Boyd on 7/8/17.
 //  Copyright © 2017 Facebook. All rights reserved.
 //
 
-#import "UIImage+CCHelper.h"
+#import "UIImage+CCMHelper.h"
 
-@implementation UIImage (CCHelper)
+@implementation UIImage (CCMHelper)
 
-- (UIImage *)tintWithColor:(UIColor *)color
+- (UIImage *)CCMTintWithColor:(UIColor *)color
 {
     UIGraphicsBeginImageContextWithOptions(self.size, NO, 0.0);
-    
+
     CGRect rect = CGRectZero;
     rect.size = self.size;
-    
+
     [color set];
     UIRectFill(rect);
-    
+
     [self drawInRect:rect blendMode:kCGBlendModeDestinationIn alpha:1.0];
-    
+
     UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
     return newImage;
 }
 
-- (UIImage *)tintWhite
+- (UIImage *)CCMTintWhite
 {
-    return [self tintWithColor:[UIColor whiteColor]];
+    return [self CCMTintWithColor:[UIColor whiteColor]];
 }
 
-+ (UIImage *)imageFromColor:(UIColor *)color forSize:(CGSize)size
++ (UIImage *)CCMImageFromColor:(UIColor *)color forSize:(CGSize)size
 {
     CGRect rect = CGRectMake(0, 0, size.width, size.height);
     UIGraphicsBeginImageContext(rect.size);
-    
+
     CGContextRef context = UIGraphicsGetCurrentContext();
     CGContextSetFillColorWithColor(context, [color CGColor]);
     CGContextFillRect(context, rect);
-    
+
     UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
-    
+
     // Begin a new image that will be the new image with the rounded corners
     // (here with the size of an UIImageView)
     UIGraphicsBeginImageContext(size);
-    
+
     // Add a clip before drawing anything, in the shape of an rounded rect
     [[UIBezierPath bezierPathWithRoundedRect:rect cornerRadius:0] addClip];
     // Draw your image
     [image drawInRect:rect];
-    
+
     // Get the image, here setting the UIImageView image
     image = UIGraphicsGetImageFromCurrentImageContext();
-    
+
     // Lets forget about that we were drawing
     UIGraphicsEndImageContext();
-    
+
     return image;
 }
 
-- (UIImage *) croppedImage:(CGRect)cropRect
+- (UIImage *) CCMCroppedImage:(CGRect)cropRect
 {
-    return [self croppedImage:cropRect forOrientation:self.imageOrientation];
+    return [self CCMCroppedImage:cropRect forOrientation:self.imageOrientation];
 }
 
-- (UIImage *)croppedImage:(CGRect)cropRect forOrientation:(UIImageOrientation)orientation {
-    
+- (UIImage *)CCMCroppedImage:(CGRect)cropRect forOrientation:(UIImageOrientation)orientation {
+
     CGImageRef imageRef = CGImageCreateWithImageInRect( [self CGImage], cropRect );
     UIImage *croppedImage = [UIImage imageWithCGImage:imageRef scale:1.0f orientation:orientation];
     CGImageRelease(imageRef);
     return croppedImage;
 }
 
-- (UIImage *) rotateUIImage
+- (UIImage *) CCMRotateUIImage
 {
     // No-op if the orientation is already correct
     if (self.imageOrientation == UIImageOrientationUp) {
         return self;
     }
-    
+
     // Otherwise, rotate the image based on it's imageOrientation
     else {
-        return [self rotateForImageOrientation:self.imageOrientation];
+        return [self CCMRotateForImageOrientation:self.imageOrientation];
     }
 }
 
-- (UIImage *)rotateForImageOrientation:(UIImageOrientation)orientation {
-    
+- (UIImage *)CCMRotateForImageOrientation:(UIImageOrientation)orientation {
+
     // We need to calculate the proper transformation to make the image upright.
     // We do it in 2 steps: Rotate if Left/Right/Down, and then flip if Mirrored.
     CGAffineTransform transform = CGAffineTransformIdentity;
-    
+
     switch (orientation) {
         case UIImageOrientationDown:
             transform = CGAffineTransformTranslate(transform, (self.size.width + (fabs(self.size.height - self.size.width))/2), (self.size.height - (fabs(self.size.height - self.size.width))/2));
             transform = CGAffineTransformRotate(transform, M_PI);
             break;
-            
+
         case UIImageOrientationDownMirrored:
             transform = CGAffineTransformTranslate(transform, (self.size.width - (fabs(self.size.height - self.size.width))/2), (self.size.height - (fabs(self.size.height - self.size.width))/2));
             transform = CGAffineTransformRotate(transform, M_PI);
             break;
-            
+
         case UIImageOrientationLeft:
         case UIImageOrientationLeftMirrored:
             transform = CGAffineTransformTranslate(transform, self.size.width, 0);
             transform = CGAffineTransformRotate(transform, M_PI_2);
             break;
-            
+
         case UIImageOrientationRight:
         case UIImageOrientationRightMirrored:
             transform = CGAffineTransformTranslate(transform, 0, self.size.height);
             transform = CGAffineTransformRotate(transform, -M_PI_2);
             break;
-            
+
         case UIImageOrientationUp:
             transform = CGAffineTransformTranslate(transform, -(fabs(self.size.height - self.size.width))/2, (fabs(self.size.height - self.size.width))/2);
             break;
-            
+
         case UIImageOrientationUpMirrored:
             transform = CGAffineTransformTranslate(transform, (fabs(self.size.height - self.size.width))/2, (fabs(self.size.height - self.size.width))/2);
             break;
     }
-    
+
     switch (orientation) {
         case UIImageOrientationUpMirrored:
         case UIImageOrientationDownMirrored:
             transform = CGAffineTransformTranslate(transform, self.size.width, 0);
             transform = CGAffineTransformScale(transform, -1, 1);
             break;
-            
+
         case UIImageOrientationLeftMirrored:
         case UIImageOrientationRightMirrored:
             transform = CGAffineTransformTranslate(transform, self.size.height, 0);
@@ -144,17 +144,17 @@
         case UIImageOrientationRight:
             break;
     }
-    
+
     // Now we draw the underlying CGImage into a new context, applying the transform
     // calculated above.
     CGContextRef ctx = CGBitmapContextCreate(NULL, self.size.width, self.size.height,
                                              CGImageGetBitsPerComponent(self.CGImage), 0,
                                              CGImageGetColorSpace(self.CGImage),
                                              CGImageGetBitmapInfo(self.CGImage));
-    
+
     CGContextConcatCTM(ctx, transform);
     CGContextDrawImage(ctx, CGRectMake(0,0,self.size.height,self.size.width), self.CGImage);
-    
+
     // And now we just create a new UIImage from the drawing context
     CGImageRef cgimg = CGBitmapContextCreateImage(ctx);
     UIImage *img = [UIImage imageWithCGImage:cgimg];
@@ -163,11 +163,11 @@
     return img;
 }
 
-- (UIImage *)rotate
+- (UIImage *)CCMRotate
 {
     UIImageOrientation currentOrientation = self.imageOrientation;
     UIImageOrientation destinationOrientation = currentOrientation;
-    
+
     if (currentOrientation == UIImageOrientationUp)
     {
         destinationOrientation = UIImageOrientationRight;
@@ -188,38 +188,38 @@
     {
         destinationOrientation = currentOrientation;
     }
-    
+
     return [[UIImage alloc] initWithCGImage:[self CGImage] scale:1.f orientation:destinationOrientation];
 }
 
-- (UIImage *)imageRotatedByDegrees:(CGFloat)degrees
+- (UIImage *)CCMImageRotatedByDegrees:(CGFloat)degrees
 {
     // calculate the size of the rotated view's containing box for our drawing space
     UIView *rotatedViewBox = [[UIView alloc] initWithFrame:CGRectMake(0,0,self.size.width, self.size.height)];
     CGAffineTransform t = CGAffineTransformMakeRotation((M_PI/180)*degrees);
     rotatedViewBox.transform = t;
     CGSize rotatedSize = rotatedViewBox.frame.size;
-    
+
     // Create the bitmap context
     UIGraphicsBeginImageContext(rotatedSize);
     CGContextRef bitmap = UIGraphicsGetCurrentContext();
-    
+
     // Move the origin to the middle of the image so we will rotate and scale around the center.
     CGContextTranslateCTM(bitmap, rotatedSize.width/2, rotatedSize.height/2);
-    
+
     // Rotate the image context
     CGContextRotateCTM(bitmap, (M_PI/180)*degrees);
-    
+
     // Now, draw the rotated/scaled image into the context
     CGContextScaleCTM(bitmap, 1.0, -1.0);
     CGContextDrawImage(bitmap, CGRectMake(-self.size.width / 2, -self.size.height / 2, self.size.width, self.size.height), [self CGImage]);
-    
+
     UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
     return newImage;
 }
 
-- (UIImage *)scaledToSize:(CGSize)newSize
+- (UIImage *)CCMScaledToSize:(CGSize)newSize
 {
     UIGraphicsBeginImageContextWithOptions(newSize, NO, 1.0);
     [self drawInRect:CGRectMake(0, 0, newSize.width, newSize.height)];
