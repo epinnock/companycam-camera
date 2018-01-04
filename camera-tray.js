9c80c9c3238b2c20f6edc69e382a6ec3f7c0752c
jaredgoertzen <jaredgoertzen@gmail.com>
2018-01-04 10:00:40 -0600

    lots of style tweaks to tray

--- camera-tray.js
+++ camera-tray.js
@@ -7,40 +7,40 @@ import styled from 'styled-components/native';
 
 const SCROLLPADDING = 8;
 const TRAYITEMHEIGHT = 80;
-const cloudIconOn = (
-  // <MaterialIcon name="cloud-queue" size={18} color="white" />
-  <MaterialIcon name="check" size={18} color="white" />
-);
-const cloudIconOff = (
-  <MaterialIcon name="cloud-off" size={18} color="rgba(255,255,255,0.8)" />
-);
-const editedIconOn = (
-  <MaterialIcon name="create" size={16} color="rgba(255,255,255,0.8)" />
-);
+
 const hasCommentsIcon = (
-  <MaterialIcon name="chat-bubble-outline" size={16} color="rgba(255,255,255,0.8)" />
+  <MaterialIcon name="chat-bubble" size={11} style={{marginHorizontal: 2}} color="white" />
 );
 const hasTagsIcon = (
-  <MaterialIcon name="bookmark-border" size={16} color="rgba(255,255,255,0.8)" />
+  <MaterialIcon name="local-offer" size={11} style={{marginHorizontal: 2}} color="white" />
+);
+const gestureIcon = (
+  <MaterialIcon name="gesture" size={11} style={{marginHorizontal: 2}} color="white" />
 );
 const chevronDown = (
   <MaterialIcon name="keyboard-arrow-down" size={18} style={{marginRight: 4}} color="white" />
 );
-
-const ImageTray = styled.View`
-  ${''/* background-color: #263238; */}
-  background-color: rgba(38,50,56, 0.5);
-`;
+const retryIcon = (
+  <MaterialIcon name="sync-problem" size={12} color="white" />
+);
+const pencilIcon = (
+  <MaterialIcon name="mode-edit" size={12} style={{marginLeft: 8 }} color="white" />
+);
 
 const ImageTrayActionBar = styled.View`
   flex-direction: row;
   justify-content: space-between;
-  padding-top: 8px;
-  padding-right: 16px;
-  padding-bottom: 8px;
-  padding-left: 16px;
+  padding: 0px 16px 8px 16px;
   ${''/* background-color: #37474F; */}
-  background-color: rgba(55,71,79, 0.5);
+  ${''/* background-color: rgba(55,71,79, 0.5); */}
+`;
+
+const ImageTrayFileControl = styled.View`
+  flex-direction: row;
+  alignItems: center;
+  justifyContent: space-between;
+  padding: 8px 16px;
+  background-color: rgba(38,50,56, 0.5);
 `;
 
 const ImageTrayItem = styled.Image`
@@ -48,33 +48,53 @@ const ImageTrayItem = styled.Image`
   height: ${TRAYITEMHEIGHT}px;
   width: ${TRAYITEMHEIGHT}px;
   margin-left: 8px;
-  border-width: ${props => props.active ? '2px' : '0'};
-  border-color: ${props => props.active ? 'white' : 'transparent'};
 `;
 
 const EmptyStateContent = styled.View`
   align-items: center;
   justify-content: center;
+  background-color: rgba(38,50,56, 0.5),
   height: ${TRAYITEMHEIGHT + SCROLLPADDING * 2}px;
 `;
 
 const IconContainer = styled.View`
   align-items: center;
   justify-content: center;
-  width: 24px;
-  height: 24px;
 `;
 
 const IconContainerCircle = IconContainer.extend`
-  margin: 4px;
-  borderRadius: 12px;
-  background-color: 'rgba(0,0,0,0.3)';
+  borderRadius: 9px;
+  width: 18px;
+  height: 18px;
+  background-color: ${props => props.waiting ? 'rgba(255,193,7,0.8)' : 'rgba(0,0,0,0.5)'};
 `;
 
 const styles = StyleSheet.create({
+  iconContainer: {
+    flex: 1,
+    justifyContent: 'space-between',
+    padding: 4,
+  },
+  iconTray: {
+    alignSelf: 'flex-end',
+  },
+  iconTrayPill: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    height: 18,
+    borderTopLeftRadius: 9,
+    borderBottomLeftRadius: 9,
+    borderTopRightRadius: 9,
+    borderBottomRightRadius: 9,
+    paddingHorizontal: 4,
+    paddingVertical: 2,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  },
   itemScroller: {
     paddingVertical: SCROLLPADDING,
     paddingRight: SCROLLPADDING,
+    backgroundColor: 'rgba(38,50,56, 0.5)',
   },
   emptyStateText: {
     textAlign: 'center',
@@ -86,62 +106,64 @@ const styles = StyleSheet.create({
 
 class CameraTray extends Component {
 
-  renderTrayIconFromData = (trayItem) => {
+  renderIconTray = (trayItem) => {
+    const trayItemIcons = [];
+
+    if (trayItem.edited) {
+      trayItemIcons.push(gestureIcon);
+    }
+
+    if (trayItem.hasComments) {
+      trayItemIcons.push(hasCommentsIcon);
+    }
+
+    if (trayItem.hasTags) {
+      trayItemIcons.push(hasTagsIcon);
+    }
+
+    if (trayItemIcons.length === 0) {
+      return null;
+    }
+
+    return (
+      <View style={styles.iconTray}>
+        { trayItemIcons.length === 1 ?
+          <IconContainerCircle>
+            {trayItemIcons}
+          </IconContainerCircle> :
+          <View style={styles.iconTrayPill}>
+            {trayItemIcons}
+          </View>
+        }
+      </View>
+    );
+  }
+
+
+  renderTrayImageFromData = (trayItem) => {
     return (
       <TouchableOpacity
         onPress={() => { this.props.onSelectTrayItem(trayItem); }}
       >
         <ImageTrayItem
+          failure={ !trayItem.uploaded }
           source={{ uri: trayItem.url }}
-          style={{ flexWrap: 'wrap', borderRadius: Platform === 'ios' ? 4 : 0 }}
+          style={{ borderRadius: Platform === 'ios' ? 4 : 0 }}
           resizeMode="cover"
         >
-          <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
-            { trayItem.uploaded ?
-                <IconContainerCircle>
-                  {cloudIconOn}
+          <View style={styles.iconContainer}>
+            {/* retryIcon container */}
+            <View>
+              {
+                !trayItem.uploaded ?
+                <IconContainerCircle waiting>
+                  {retryIcon}
                 </IconContainerCircle> :
                 null
-            }
-          </View>
-
-          <View style={{ flexDirection: 'row', justifyContent: 'flex-end', backgroundColor: 'rgba(0,0,0,0.2)' }}>
-            {/* <IconContainer style={{ width: '33.33%' }}>
-              {editedIconOn}
-            </IconContainer>
-            <IconContainer style={{ width: '33.33%' }}>
-              {hasCommentsIcon}
-            </IconContainer>
-            <IconContainer style={{ width: '33.33%' }}>
-              {hasTagsIcon}
-            </IconContainer> */}
-            {
-              trayItem.edited ?
-                <IconContainer
-                  style={{ width: '33.33%' }}
-                >
-                  {editedIconOn}
-                </IconContainer> :
-                null
-            }
-            {
-              trayItem.hasComments ?
-                <IconContainer
-                  style={{ width: '33.33%' }}
-                >
-                  {hasCommentsIcon}
-                </IconContainer> :
-                null
-            }
-            {
-              trayItem.hasTags ?
-                <IconContainer
-                  style={{ width: '33.33%' }}
-                >
-                  {hasTagsIcon}
-                </IconContainer> :
-                null
-            }
+              }
+            </View>
+            {/* edits, comments, tags */}
+            {this.renderIconTray(trayItem)}
           </View>
         </ImageTrayItem>
       </TouchableOpacity>
@@ -156,12 +178,12 @@ class CameraTray extends Component {
     const trayIconsEmpty = !trayItems || (trayItems.length === 0);
 
     return (
-      <ImageTray>
+      <View>
 
         <ImageTrayActionBar>
           <TouchableOpacity
             onPress={this.props.onHideTray}
-            style={{ flexDirection: 'row' }}
+            style={{ flexDirection: 'row', alignItems: 'center' }}
           >
             {chevronDown}
             <Text style={{ color: 'white' }}>Hide</Text>
@@ -174,6 +196,17 @@ class CameraTray extends Component {
             <Text style={{ color: 'white' }}>Done</Text>
           </TouchableOpacity> */}
         </ImageTrayActionBar>
+
+        {/* TODO will be used for scanner mode */}
+        <ImageTrayFileControl>
+          <TouchableOpacity
+            onPress={() => {}}
+            style={{ flexDirection: 'row', alignItems: 'center' }}
+          >
+            <Text style={{ color: 'rgba(255,255,255,0.8)' }}>SomeFileName.pdf</Text>
+            {pencilIcon}
+          </TouchableOpacity>
+        </ImageTrayFileControl>
 
         {trayIconsEmpty ? (
 
@@ -190,12 +223,10 @@ class CameraTray extends Component {
             showsHorizontalScrollIndicator={false}
             contentContainerStyle={styles.itemScroller}
           >
-            {trayItems.map(this.renderTrayIconFromData)}
+            {trayItems.map(this.renderTrayImageFromData)}
           </ScrollView>
-
         )}
-
-      </ImageTray>
+      </View>
     );
   }
 }
